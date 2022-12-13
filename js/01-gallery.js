70d64fa671fef09b48642689713b590002b52aba
AndriyNich <info@bembi.ua>
2022-12-13 20:48:21 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems} from './gallery-items.js'
 // Change code below this line
 
-console.log(galleryItems);
+// import * as basicLightbox from 'basiclightbox'
+
+const refGalleryContainer = document.querySelector('div.gallery')
+
+const createGallery = galleryItems =>
+  galleryItems
+    .map(
+      ({original, preview, description}) =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`,
+    )
+    .join('')
+
+const inputGalleryToDocument = innerHtml => {
+  refGalleryContainer.innerHTML = createGallery(galleryItems)
+}
+
+inputGalleryToDocument()
+
+const onGalleryClick = event => {
+  console.log(event.target.dataset.source)
+  const instance = basicLightbox
+    .create(
+      `
+    <img src="${event.target.dataset.source}" width="800" height="600">`,
+    )
+    .show()
+}
+
+refGalleryContainer.addEventListener('click', onGalleryClick)
